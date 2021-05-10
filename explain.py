59dd2f084929636c342501f75215da3eb5b4951a
ehdqoddl6 <hays39@naver.com>
2021-05-10 20:24:05 +0900

    ''

---
+++ explain.py
@@ -0,0 +1,63 @@
+import os
+import keras
+from keras.applications import inception_v3 as inc_net
+from keras.preprocessing import image
+from keras.applications.imagenet_utils import decode_predictions
+from skimage.io import imread
+import matplotlib.pyplot as plt
+import numpy as np
+import warnings
+warnings.filterwarnings(action='ignore') 
+
+
+inet_model = inc_net.InceptionV3()
+
+def transform_img_fn(path_list):
+    out = []
+    for img_path in path_list:
+        img = image.load_img(img_path, target_size=(299, 299))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = inc_net.preprocess_input(x)
+        out.append(x)
+    return np.vstack(out)
+
+images = transform_img_fn([os.path.join('image_sample/','sample_image.png')])
+
+plt.imshow(images[0] / 2 + 0.5)
+#plt.show()
+preds = inet_model.predict(images)
+#print(preds)
+
+for x in decode_predictions(preds)[0]:
+    print(x)
+
+import base
+import image
+
+
+explainer = image.ImageExplainer()
+print(images[0].astype('double').shape)
+explanation = explainer.explain_instance(images[0].astype('double'), inet_model.predict, top_labels=50, hide_color=0, num_samples=10)
+
+
+from skimage.segmentation import mark_boundaries
+"""
+temp, mask = explanation.get_image_and_mask(explanation.top_labels[0], positive_only=True, num_features=5, hide_rest=True)
+plt.imshow(mark_boundaries(temp / 2 + 0.5, mask))
+plt.show()
+"""
+temp, mask = explanation.get_image_and_mask(explanation.top_labels[0], positive_only=True, num_features=5, hide_rest=False)
+plt.imshow(mark_boundaries(temp / 2 + 0.5, mask))
+plt.show()
+plt.imsave('result/result.png', mark_boundaries(temp / 2 + 0.5, mask))
+
+"""
+temp, mask = explanation.get_image_and_mask(explanation.top_labels[0], positive_only=False, num_features=10, hide_rest=False)
+plt.imshow(mark_boundaries(temp / 2 + 0.5, mask))
+plt.show()
+
+temp, mask = explanation.get_image_and_mask(explanation.top_labels[0], positive_only=False, num_features=1000, hide_rest=False, min_weight=0.1)
+plt.imshow(mark_boundaries(temp / 2 + 0.5, mask))
+plt.show()
+"""
---
+++ image.py
@@ -0,0 +1,158 @@
+import copy
+from functools import partial
+
+import numpy as np
+import sklearn
+import sklearn.preprocessing
+from sklearn.utils import check_random_state
+from skimage.color import gray2rgb
+from tqdm.auto import tqdm
+from skimage.segmentation import felzenszwalb, slic, quickshift, watershed
+
+import base
+from scikit_image import SegmentationAlgorithm
+
+
+class ImageExplanation(object):
+
+    def __init__(self, image, segments):
+        
+        self.image = image
+        self.segments = segments
+        self.intercept = {}
+        self.local_exp = {}
+        self.local_pred = None
+
+    def get_image_and_mask(self, label, positive_only=True, negative_only=False, hide_rest=False, num_features=5, min_weight=0.):
+        
+        if label not in self.local_exp:
+            raise KeyError('Label not in explanation')
+        if positive_only & negative_only:
+            raise ValueError("Positive_only and negative_only cannot be true at the same time.")
+
+        segments = self.segments
+        image = self.image
+        exp = self.local_exp[label]
+        mask = np.zeros(segments.shape, segments.dtype)
+
+        if hide_rest:
+            temp = np.zeros(self.image.shape)
+        else:
+            temp = self.image.copy()
+        if positive_only:
+            fs = [x[0] for x in exp
+                  if x[1] > 0 and x[1] > min_weight][:num_features]
+        if negative_only:
+            fs = [x[0] for x in exp
+                  if x[1] < 0 and abs(x[1]) > min_weight][:num_features]
+        if positive_only or negative_only:
+            for f in fs:
+                temp[segments == f] = image[segments == f].copy()
+                mask[segments == f] = 1
+            return temp, mask
+        else:
+            for f, w in exp[:num_features]:
+                if np.abs(w) < min_weight:
+                    continue
+                c = 0 if w < 0 else 1
+                mask[segments == f] = -1 if w < 0 else 1
+                temp[segments == f] = image[segments == f].copy()
+                temp[segments == f, c] = np.max(image)
+            return temp, mask
+
+
+class ImageExplainer(object):
+
+    def __init__(self, kernel_width=.25, kernel=None, verbose=False, feature_selection='auto', random_state=None):
+        
+        kernel_width = float(kernel_width)
+
+        if kernel is None:
+            def kernel(d, kernel_width):
+                return np.sqrt(np.exp(-(d ** 2) / kernel_width ** 2))
+
+        kernel_fn = partial(kernel, kernel_width=kernel_width)
+
+        self.random_state = check_random_state(random_state)
+        self.feature_selection = feature_selection
+        self.base = base.Base(kernel_fn, verbose, random_state=self.random_state)
+
+    def explain_instance(self, image, classifier_fn, labels=(1,),
+                         hide_color=None,
+                         top_labels=5, num_features=100000, num_samples=1000,
+                         batch_size=10,
+                         segmentation_fn=None,
+                         distance_metric='cosine',
+                         model_regressor=None,
+                         random_seed=None,
+                         progress_bar=True):
+       
+        if len(image.shape) == 2:
+            image = gray2rgb(image)
+
+        if random_seed is None:
+            random_seed = self.random_state.randint(0, high=1000)
+
+        if segmentation_fn is None:
+            segmentation_fn = SegmentationAlgorithm('quickshift', kernel_size=4,
+                                                    max_dist=200, ratio=0.2,
+                                                    random_seed=random_seed)
+        try:
+            segments = segmentation_fn(image)
+        except ValueError as e:
+            raise e
+
+        fudged_image = image.copy()
+        if hide_color is None:
+            for x in np.unique(segments):
+                fudged_image[segments == x] = (
+                    np.mean(image[segments == x][:, 0]),
+                    np.mean(image[segments == x][:, 1]),
+                    np.mean(image[segments == x][:, 2]))
+        else:
+            fudged_image[:] = hide_color
+
+        top = labels
+
+        data, labels = self.data_labels(image, fudged_image, segments, classifier_fn, num_samples, batch_size=batch_size, progress_bar=progress_bar)
+
+        distances = sklearn.metrics.pairwise_distances( data, data[0].reshape(1, -1), metric=distance_metric).ravel()
+
+        ret_exp = ImageExplanation(image, segments)
+        
+        if top_labels:
+            top = np.argsort(labels[0])[-top_labels:]
+            ret_exp.top_labels = list(top)
+            ret_exp.top_labels.reverse()
+        for label in top:
+            (ret_exp.intercept[label],
+             ret_exp.local_exp[label],
+             ret_exp.score, ret_exp.local_pred) = self.base.explain_instance_with_data(data, labels, distances, label, num_features, model_regressor=model_regressor, feature_selection=self.feature_selection)
+        return ret_exp
+
+    def data_labels(self, image, fudged_image, segments, classifier_fn, num_samples, batch_size=10, progress_bar=True):
+        
+        n_features = np.unique(segments).shape[0]
+        data = self.random_state.randint(0, 2, num_samples * n_features)\
+            .reshape((num_samples, n_features))
+            
+        labels = []
+        data[0, :] = 1
+        imgs = []
+        rows = tqdm(data) if progress_bar else data
+        for row in rows:
+            temp = copy.deepcopy(image)
+            zeros = np.where(row == 0)[0]
+            mask = np.zeros(segments.shape).astype(bool)
+            for z in zeros:
+                mask[segments == z] = True
+            temp[mask] = fudged_image[mask]
+            imgs.append(temp)
+            if len(imgs) == batch_size:
+                preds = classifier_fn(np.array(imgs))
+                labels.extend(preds)
+                imgs = []
+        if len(imgs) > 0:
+            preds = classifier_fn(np.array(imgs))
+            labels.extend(preds)
+        return data, np.array(labels)
